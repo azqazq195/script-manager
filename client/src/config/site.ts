73fe2f22b4f906af0f02e939b06abca302fec663
moseoh <azqazq195@gmail.com>
2024-01-13 22:21:42 +0900

    feat: config 추가

---
+++ client/src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "script manager",
+  url: "https://ui.shadcn.com",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description:
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  links: {
+    github: "https://github.com/azqazq195/script-manager",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
